59dae6ed11dd8615eab8a0ae2259343cad039716
Krivenko Dmitriy <Krivenko.Dmitriy>
2020-12-23 21:22:32 +0300

    Start logging

--- lesson-7/main.go
+++ lesson-7/main.go
@@ -2,14 +2,80 @@ package main
 
 import (
 	"context"
+	"google.golang.org/grpc/metadata"
 	"log"
+	"time"
 )
 
-func main() {
-	ctx, _ := context.WithCancel(context.Background())
-	err := StartMyMicroservice(ctx, "127.0.0.1:8082", "")
+var acl string = `{
+	"logger":    ["/main.Admin/Logging"],
+	"stat":      ["/main.Admin/Statistics"],
+	"biz_user":  ["/main.Biz/Check", "/main.Biz/Add"],
+	"biz_admin": ["/main.Biz/*"]
+}`
+func mainWait(amout int) {
+	time.Sleep(time.Duration(amout) * 10 * time.Millisecond)
+}
+
+
+func getConsumer(consumerName string) context.Context {
+	// ctx, _ := context.WithTimeout(context.Background(), time.Second)
+	ctx := context.Background()
+	md := metadata.Pairs(
+		"consumer", consumerName,
+	)
+	return metadata.NewOutgoingContext(ctx, md)
+}
+
+
+// старт-стоп сервера
+func TestStartStop() {
+	ctx, finish := context.WithCancel(context.Background())
+	err := StartMyMicroservice(ctx, "127.0.0.1:8082", acl)
 	if err != nil {
 		log.Fatalf("cant start server initial: %v", err)
 	}
-	println("usage: go test -v")
+	mainWait(1)
+	finish() // при вызове этой функции ваш сервер должен остановиться и освободить порт
+	mainWait(1)
+	// теперь проверим что вы освободили порт и мы можем стартовать сервер ещё раз
+	ctx, finish = context.WithCancel(context.Background())
+	err = StartMyMicroservice(ctx, "127.0.0.1:8082", acl)
+	if err != nil {
+		log.Fatalf("cant start server again: %v", err)
+	}
+	mainWait(1)
+	finish()
+	mainWait(1)
+}
+
+// TODO перепроверить в конце
+//func TestLeak() {
+//	goroutinesStart := runtime.NumGoroutine()
+//	TestStartStop()
+//	goroutinesPerTwoIterations := runtime.NumGoroutine() - goroutinesStart
+//
+//	goroutinesStart = runtime.NumGoroutine()
+//	goroutinesStat := []int{}
+//	for i := 0; i <= 25; i++ {
+//		TestStartStop()
+//		goroutinesStat = append(goroutinesStat, runtime.NumGoroutine())
+//	}
+//	goroutinesPerFiftyIterations := runtime.NumGoroutine() - goroutinesStart
+//	if goroutinesPerFiftyIterations > goroutinesPerTwoIterations*5 {
+//		log.Fatalf("looks like you have goroutines leak: %+v", goroutinesStat)
+//	}
+//}
+
+func main() {
+	//TestLeak()
+	ctx, _ := context.WithCancel(context.Background())
+	ctx = getConsumer("test")
+	err := StartMyMicroservice(ctx, "127.0.0.1:8082", acl)
+	if err != nil {
+		log.Fatalf("cant start server initial: %v", err)
+	}
+
+	time.Sleep(time.Hour)
+
 }
--- lesson-7/service.go
+++ lesson-7/service.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 	"log"
 	"net"
+	"regexp"
 )
+
 
 type AdminService struct {
 	UnimplementedAdminServer
@@ -20,25 +26,68 @@ type Permissions struct {
 	Data map[string][]string
 }
 
-func (acl *Permissions) checkPermission(
+// checkUnaryPermission - check permissions for unary methods
+func (acl *Permissions) checkUnaryPermission(
 	ctx context.Context,
 	req interface{},
 	info *grpc.UnaryServerInfo,
 	handler grpc.UnaryHandler) (interface{}, error) {
-	// TODO тут провести валидацию по данным полученным из контекста и того что есть в структуре
-	fmt.Println("Check permissions", info.FullMethod)
-	reply, err := handler(ctx, req)
-	return reply, err
+	md, _ := metadata.FromIncomingContext(ctx)
+	consumer := md.Get("consumer")
+	if len(consumer) != 0 {
+		allowedMethods := acl.Data[consumer[0]]
+		for _, m := range allowedMethods {
+			matched, err := regexp.Match(m, []byte(info.FullMethod))
+			if err != nil {
+				return nil, status.Error(codes.Internal, "internal server error")
+			}
+			if matched {
+				return handler(ctx, req)
+			}
+		}
+	}
+	return nil, status.Error(codes.Unauthenticated, "unknown consumer")
+}
+
+// checkStreamPermission - check permissions for streaming method
+func (acl *Permissions) checkStreamPermission(
+	srv interface{},
+	ss grpc.ServerStream,
+	info *grpc.StreamServerInfo,
+	handler grpc.StreamHandler) error {
+	md, _ := metadata.FromIncomingContext(ss.Context())
+	consumer := md.Get("consumer")
+	if len(consumer) != 0 {
+		allowedMethods := acl.Data[consumer[0]]
+		for _, m := range allowedMethods {
+			matched, err := regexp.Match(m, []byte(info.FullMethod))
+			if err != nil {
+				return status.Error(codes.Internal, "internal server error")
+			}
+			if matched {
+				return handler(srv, ss)
+			}
+		}
+	}
+	return status.Error(codes.Unauthenticated, "unknown consumer")
 }
 
 // ================================ Admin methods ================================
-
 
 func NewAdminService() *AdminService {
 	return &AdminService{}
 }
 
 func (as *AdminService) Logging(n *Nothing, server Admin_LoggingServer) error {
+	// Добавить сервер в поле структуры
+	// Если там единственный сервер запустить горутину
+	go func() {
+		for {
+			// Ждем сообщение из канала
+			// Обходим массив с серверами и в каждом делаем send
+			break
+		}
+	}()
 	return nil
 }
 
@@ -53,35 +102,45 @@ func NewBizService() *BizService {
 }
 
 func (bs *BizService) Check(ctx context.Context, n *Nothing) (*Nothing, error) {
-	fmt.Println("Biz check working")
 	return &Nothing{Dummy: false}, nil
 }
 
 func (bs *BizService) Add(ctx context.Context, n *Nothing) (*Nothing, error) {
-	return nil, nil
+	return &Nothing{Dummy: false}, nil
 }
 
 func (bs *BizService) Test(ctx context.Context, n *Nothing) (*Nothing, error) {
-	return nil, nil
+	return &Nothing{Dummy: false}, nil
 }
 
 // StartMyMicroservice - start server
 func StartMyMicroservice(ctx context.Context, addr string, ACLData string) error {
-	// TODO тут распарсить ACL и сделать валидацию json. Положить в структуру
-	acl := Permissions{}
+	// Parse ACL data
+	data := map[string][]string{}
+	err := json.Unmarshal([]byte(ACLData), &data)
+	if err != nil {
+		return err
+	}
+	acl := Permissions{Data: data}
+
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalln("cant listet port", err)
 	}
 
 	server := grpc.NewServer(
-		grpc.UnaryInterceptor(acl.checkPermission),
+		grpc.UnaryInterceptor(acl.checkUnaryPermission),
+		grpc.StreamInterceptor(acl.checkStreamPermission),
 	)
 
 	RegisterAdminServer(server, NewAdminService())
 	RegisterBizServer(server, NewBizService())
 
 	fmt.Println("starting server at :8082")
-	server.Serve(lis)
+	go server.Serve(lis)
+	go func() {
+		<- ctx.Done()
+		server.Stop()
+	}()
 	return nil
 }
