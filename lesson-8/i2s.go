38d84e17c18a72f0414fcb77c1dadfaafc9d49d0
Dmitriy Krivenko <dg.krivenko@gmail.com>
2021-01-05 15:37:16 +0300

    Finish course 2

--- lesson-8/i2s.go
+++ lesson-8/i2s.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 )
 
+// validateParams - validate income parameters
 func validateParams(data interface{}, out interface{}) error {
 	// Check out type
 	outType := reflect.ValueOf(out).Type().Kind()
@@ -30,6 +31,7 @@ func validateParams(data interface{}, out interface{}) error {
 	return nil
 }
 
+// copySimple - copy simple object
 func copySimple(data interface{}, out interface{}) error {
 	source := data.(map[string]interface{})
 	res := reflect.ValueOf(out).Elem()
@@ -42,6 +44,7 @@ func copySimple(data interface{}, out interface{}) error {
 		expectedType := res.Type().Field(i).Type.Kind()
 		sourceType := reflect.TypeOf(sourceVal).Kind()
 
+		// Recursive copy of complex object
 		if expectedType == reflect.Struct || expectedType == reflect.Slice {
 			val := reflect.New(res.Field(i).Type()).Interface()
 			if err := i2s(sourceVal, val); err != nil {
@@ -51,6 +54,7 @@ func copySimple(data interface{}, out interface{}) error {
 			continue
 		}
 
+		// json convert int to float64
 		if expectedType == reflect.Int && sourceType == reflect.Float64 {
 			val := sourceVal.(float64)
 			sourceVal = int(val)
@@ -71,7 +75,7 @@ func i2s(data interface{}, out interface{}) error {
 		return err
 	}
 	switch data.(type) {
-	case []interface{}:
+	case []interface{}: // slice case
 		arr := data.([]interface{})
 		for _, item := range arr {
 			t := reflect.New(reflect.TypeOf(out).Elem().Elem()).Interface()
@@ -80,7 +84,7 @@ func i2s(data interface{}, out interface{}) error {
 			}
 			reflect.ValueOf(out).Elem().Set(reflect.Append(reflect.ValueOf(out).Elem(), reflect.ValueOf(t).Elem()))
 		}
-	case map[string]interface{}:
+	case map[string]interface{}: // simple case
 		if err := copySimple(data, out); err != nil {
 			return err
 		}
